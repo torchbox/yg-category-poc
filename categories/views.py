ec8041dc0db12c70a5021ae49b2fee24e5ecf5cb
Matt Westcott <matthew@torchbox.com>
2021-06-17 14:32:10 +0000

    Complete virtual type chooser

--- categories/views.py
+++ categories/views.py
@@ -1,13 +1,19 @@
 from django.conf.urls import url
+from django.core.exceptions import ObjectDoesNotExist
 from django.utils.translation import gettext_lazy as _
 import requests
 
 from generic_chooser.views import DRFChooserMixin, DRFChooserViewSet
 
 class VirtualTypeChooserMixin(DRFChooserMixin):
-    def get_object_list(self, **kwargs):
-        params = self.get_api_parameters(**kwargs)
-        result = requests.get(self.api_base_url, params=params).json()
+    def get_object_list(self, search_term=None, **kwargs):
+        result = requests.get(self.api_base_url).json()
+        if search_term:
+            result = [
+                item for item in result
+                if search_term in item['name']
+            ]
+
         return result
 
     def get_object_id(self, item):
@@ -16,11 +22,19 @@ class VirtualTypeChooserMixin(DRFChooserMixin):
     def get_object_string(self, item):
         return item['name']
 
+    def get_object(self, id):
+        object_list = self.get_object_list()
+        for item in object_list:
+            if item['uuid'] == id:
+                return item
+
+        raise ObjectDoesNotExist()
 
 class VirtualTypeChooserViewSet(DRFChooserViewSet):
     # base_choose_view_class = VirtualTypeChooseView
     chooser_mixin_class = VirtualTypeChooserMixin
     api_base_url = 'https://yougov.co.uk/_pubapis/v5/uk/entities/virtual_types/'
+    is_searchable = True
 
     def get_urlpatterns(self):
         return super().get_urlpatterns() + [
--- categories/widgets.py
+++ categories/widgets.py
@@ -1,8 +1,22 @@
+from django.core.exceptions import ObjectDoesNotExist
+import requests
 from generic_chooser.widgets import DRFChooser
 
 class VirtualTypeChooser(DRFChooser):
     choose_modal_url_name = 'virtual_type_chooser:choose'
     api_base_url = 'https://yougov.co.uk/_pubapis/v5/uk/entities/virtual_types/'
 
-    #def get_title(self, instance):
-    #    return instance['title']
+    def get_title(self, instance):
+        return instance['name']
+
+    def get_object_list(self):
+        result = requests.get(self.api_base_url).json()
+        return result
+
+    def get_instance(self, id):
+        object_list = self.get_object_list()
+        for item in object_list:
+            if item['uuid'] == id:
+                return item
+
+        raise ObjectDoesNotExist()
